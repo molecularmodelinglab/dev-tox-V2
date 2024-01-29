77116a0d34fa0f87a27b1ce12e57da7b2cfbd282
ricardotieghi <ricardo.tieghi@gmail.com>
2024-01-29 00:28:40 -0500

    commit

--- dev_tox/dev_tox.py
+++ dev_tox/dev_tox.py
@@ -28,9 +28,7 @@ MODEL_DICT = {
     'Third Trimester Toxicity': ['DT_third_trimester_model.joblib'],
 }
 
-# lol I'm just like screw code reada
-#
-# bility sorry
+# lol I'm just like screw code readability sorry
 MODEL_DICT_INVERT = {v: key for key, val in MODEL_DICT.items() for v in val}
 
 CLASSIFICATION_DICT = {
@@ -116,9 +114,9 @@ def main(smi, calculate_ad=True, make_prop_img=False, **kwargs):
     for key, val in kwargs.items():
         if key in MODEL_DICT.keys():  # check if this kwarg is for a model
             if val:  # check if model is turned on
-                model = MODEL_DICT[key]
-                print(model)
-                joblib.load(model)  # load model
+                model_file = MODEL_DICT[key][0]  # Get the first (and presumably only) file in the list
+                print(model_file)
+                model = joblib.load(model_file)  # load model  # load model
 
                 pred, pred_proba, ad = run_prediction(model, smi, calculate_ad=calculate_ad)
 
